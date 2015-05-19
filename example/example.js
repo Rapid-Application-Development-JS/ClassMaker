60bcda7967051fd748e9ac4fa10ed8571f777150
Anton Trofimenko (Mariupol) <a.trofimenko@mobidev.biz>
2015-05-19 17:27:58 +0300

    Update readme.

--- example/example.js
+++ example/example.js
@@ -1,42 +1,33 @@
 var ClassMaker = require("ClassMaker");
-
 /**
  * @constructor
  */
 function Animal(name) {
-
-    this.name = name||"Undefined Animal";
-    this.getAbility = function () {
-        return "I can run";
-    };
-    this.whoAreYou = function () {
-        return "I am " + this.name + ", " + this.getAbility()
-    }
+  this.name = name || "Undefined Animal";
+  this.getAbility = function () {
+    return "I can run";
+  };
+  this.whoAreYou = function () {
+    return "I am " + this.name + ", " + this.getAbility()
+  }
 }
-
 var Dog = ClassMaker.extend(new Animal("Dog"));
 var dog = new Dog;
 console.log(dog.whoAreYou());
-
-
 var bird = {
-    ability: "fly",
-    getAbility: function () {
-        return "I can "+ this.ability;
-    }
+  ability: "fly",
+  getAbility: function () {
+    return "I can " + this.ability;
+  }
 };
-
 var Duck = ClassMaker.extend(new Animal("Duck"));
 Duck = Duck.extend(bird);
 var duck = new Duck;
 console.log(duck.whoAreYou());
-
-
 var SwimDuck = Duck.extend({
-    whoAreYou: function() {
-        return this._super()+ ", and swim"; //call parent method
-    }
+  whoAreYou: function () {
+    return this._super() + ", and swim"; //call parent method
+  }
 });
-
-swimDuck  = new SwimDuck;
+swimDuck = new SwimDuck;
 console.log(swimDuck.whoAreYou());
